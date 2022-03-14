784be677a00b11210eb130fa22dfe0e23f99dd43
Felipe <fquiceno1@misena.edu.co>
2022-03-13 19:52:32 -0500

    creamos la estructura de todos los identificadores del agregado Usuario

--- src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/usuario/values/UsuarioId.java
+++ src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/usuario/values/UsuarioId.java
@@ -3,4 +3,16 @@ package co.com.sofka.tallerautomotriz.mantenimiento.usuario.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class UsuarioId extends Identity {
+    //constructor vacio
+    public UsuarioId(){
+
+    }
+    private UsuarioId(String id){
+        super(id);
+    }
+
+    //metodo para pedir el identificador sin necesidad de new
+    public static UsuarioId of(String id){
+        return new UsuarioId(id);
+    }
 }
