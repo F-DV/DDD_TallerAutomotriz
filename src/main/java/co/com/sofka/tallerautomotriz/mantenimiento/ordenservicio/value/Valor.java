24c841132f782515361747e93b903adfe9c7a61a
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-03-14 00:07:56 -0500

    Se crea objeto de valor, Valor

--- src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/ordenservicio/value/Valor.java
+++ src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/ordenservicio/value/Valor.java
@@ -1,4 +1,23 @@
 package co.com.sofka.tallerautomotriz.mantenimiento.ordenservicio.value;
 
-public class Valor {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Valor implements ValueObject<Double> {
+    private final Double value;
+
+    public Valor(Double value){
+        this.value = Objects.requireNonNull(value);
+        if(this.value <= 0){
+            throw new IllegalArgumentException("Debe tener un valido");
+        }
+
+    }
+
+    @Override
+    public Double value() {
+        return value;
+    }
+
 }
