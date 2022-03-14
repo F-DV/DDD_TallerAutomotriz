44d7e0efbc24922c3d05d5a217c99d8df9d2c300
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-03-14 08:29:28 -0500

    Se crea la entiedad Factura

--- src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/ordenservicio/Factura.java
+++ src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/ordenservicio/Factura.java
@@ -2,6 +2,35 @@ package co.com.sofka.tallerautomotriz.mantenimiento.ordenservicio;
 
 import co.com.sofka.domain.generic.Entity;
 import co.com.sofka.tallerautomotriz.mantenimiento.ordenservicio.value.FacturaId;
+import co.com.sofka.tallerautomotriz.mantenimiento.ordenservicio.value.Fecha;
+import co.com.sofka.tallerautomotriz.mantenimiento.ordenservicio.value.Valor;
 
 public class Factura extends Entity<FacturaId> {
+    private Valor valor;
+    private Fecha fecha;
+
+    public Factura(FacturaId entityId, Valor valor, Fecha fecha) {
+        super(entityId);
+        this.valor = valor;
+        this.fecha = fecha;
+    }
+
+    //Comportamientos
+
+    public void agregarPrecio(Valor valor){
+        this.valor = valor;
+    }
+
+    public void agregarFecha(Fecha fecha){
+        this.fecha = fecha;
+    }
+
+    //Propiedades
+    public Valor valor(){
+        return valor;
+    }
+
+    public Fecha fecha(){
+        return fecha;
+    }
 }
