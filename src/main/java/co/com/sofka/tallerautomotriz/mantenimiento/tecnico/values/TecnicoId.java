4f559ce9078f751431eb1a8ce7033d083ee021d9
Felipe <fquiceno1@misena.edu.co>
2022-03-29 15:01:28 -0500

    Se crea las entidades de  agregado Tecnico y sus identitys

---
+++ src/main/java/co/com/sofka/tallerautomotriz/mantenimiento/tecnico/values/TecnicoId.java
@@ -0,0 +1,15 @@
+package co.com.sofka.tallerautomotriz.mantenimiento.tecnico.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class TecnicoId extends Identity {
+
+    public TecnicoId() {
+
+    }
+
+    private TecnicoId(String id) {
+        super(id);
+    }
+
+}
